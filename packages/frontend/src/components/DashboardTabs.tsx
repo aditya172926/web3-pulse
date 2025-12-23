bb934078aaf2869c875569c3ac136b691afaaa30
aditya172926 <aditya26sg@gmail.com>
2025-12-23 11:43:37 +0530

    shift portfolio grid

--- packages/frontend/src/components/DashboardTabs.tsx
+++ packages/frontend/src/components/DashboardTabs.tsx
@@ -25,19 +25,12 @@ export default function DashboardTabs({ address }: Props) {
         >
           Outbound
         </button>
-        <button
-          onClick={() => setActiveTab(2)}
-          className={`px-4 py-2 -mb-px ${activeTab === 2 ? 'border-b-2 border-blue-500 font-bold' : ''}`}
-        >
-          Portfolio
-        </button>
       </div>
 
       {/* Transaction List */}
       {/* Content */}
       {activeTab === 0 && <TransactionList address={address} txnDirection={0} />}
       {activeTab === 1 && <TransactionList address={address} txnDirection={1} />}
-      {activeTab === 2 && <PortfolioGrid address={address} />}
     </div>
   );
 }
--- packages/frontend/src/pages/Portfolio.tsx
+++ packages/frontend/src/pages/Portfolio.tsx
@@ -1,7 +1,19 @@
+import PortfolioGrid from "../components/PortfolioGrid";
+import Sidebar from "../components/sidebar_panel/Sidebar";
+import { useSelectedAddress } from "../state";
+
 export default function Portfolio() {
+    const address = useSelectedAddress((state) => state.address);
     return (
         <div>
-            <p> Portfolio</p>
+            <div className='flex h-screen'>
+                <div className='flex-1 p-2 bg-slate-900 text-white'>
+                    <Sidebar />
+                </div>
+                <div className='flex-3 px-2 bg-slate-50 overflow-y-auto'>
+                    <PortfolioGrid address={address} />
+                </div>
+            </div>
         </div>
     )
 }

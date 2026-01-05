a0649a929cd671189c32626802d18449b84736a9
aditya172926 <aditya26sg@gmail.com>
2026-01-05 10:48:26 +0530

    rm log

--- packages/frontend/src/components/TransactionDetails.tsx
+++ packages/frontend/src/components/TransactionDetails.tsx
@@ -10,7 +10,6 @@ type Props = {
 
 export default function TransactionDetailsPage() {
     const txn_data = useSelectedTransaction((state) => state.transaction);
-    console.log("transaction data ", txn_data);
     const setSelectedTransaction = useSelectedTransaction((state) => state.setTransaction);
     const { receipt } = useLoaderData<Props>();
     const navigate = useNavigate();
